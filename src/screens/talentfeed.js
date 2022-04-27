bf6dbeaa0a0256a3d1c0f9d5b05ae548343ee34e
Jack Weeden <jack@ajack.org>
2022-04-27 15:40:16 +0100

    Remove invalid avatar field from query

--- src/screens/talentfeed.js
+++ src/screens/talentfeed.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-// import our Apollo libraries
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
+import { Text } from "react-native";
+import { useQuery, gql } from "@apollo/client";
 
-import TalentList from '../components/TalentList';
-import Loading from '../components/Loading';
+import TalentList from "../components/TalentList";
+import Loading from "../components/Loading";
 
 // compose our query
 const GET_TALENT_LIST = gql`
@@ -12,23 +11,24 @@ const GET_TALENT_LIST = gql`
     talentList {
       id
       name
-      avatar
     }
   }
 `;
 
-const TalentFeed = props => {
+const TalentFeed = (props) => {
   const { loading, error, data } = useQuery(GET_TALENT_LIST);
 
   if (loading) return <Loading />;
 
   if (error) return <Text>Error loading talent</Text>;
 
-  return <TalentList talentList={data.talentList} navigation={props.navigation} />;
+  return (
+    <TalentList talentList={data.talentList} navigation={props.navigation} />
+  );
 };
 
 TalentFeed.navigationOptions = {
-  title: 'Talent Feed'
+  title: "Talent Feed",
 };
 
 export default TalentFeed;
